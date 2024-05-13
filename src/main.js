86f31b4d7700229154581154766d5d0eb1534db4
aslakmal <39646507+aslakmal@users.noreply.github.com>
2024-05-13 18:52:02 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -398,7 +398,7 @@ function ccalc(xval) {
   //  xval + fval,
     xval + fval,
     xval + fval,
-  /*  xval + fval,
+    xval + fval,
     xval + fval,
     xval + fval,
     xval + fval,
@@ -492,7 +492,7 @@ function ccalc(xval) {
       xval + fval,
       xval + fval,
       xval + fval,
-      xval + fval*/
+      xval + fval
   ];
 //  updatemicdata(cal)
   createBars(cal); // Update the chart with new calculation values
