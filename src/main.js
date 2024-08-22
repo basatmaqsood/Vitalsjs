b7cdfbd805192488029ec76369abed667ede8895
aslakmal <39646507+aslakmal@users.noreply.github.com>
2024-08-23 01:09:19 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ const db = getFirestore(app);
 
 // Get a list of cities from your database
 async function getCities(db) {
-  const citiesCol = collection(db, 'cities'); 
+  const citiesCol = collection(db, 'cities');  
   const citySnapshot = await getDocs(citiesCol);
   const cityList = citySnapshot.docs.map(doc => doc.data());
   return cityList;
